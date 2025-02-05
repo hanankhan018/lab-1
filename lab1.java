193630485b4c5643226bc2ff194073ec28a4a108
hanankhan018 <bhaipthan018@gmail.com>
2025-02-05 23:23:10 +0500

    Add files via upload

---
+++ lab1.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+class lab1{
+public static void main(String[] args){
+Scanner input=new Scanner(System.in);
+
+/*
+//task 1------------------------------------------
+System.out.println("///////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+System.out.println("==\t\tStudent Points\t\t==");
+System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\//////////////////////////");
+System.out.println("Name    \t\tLab\tBonus\tTotal");
+System.out.println("----    \t\t---\t-----\t-----");
+System.out.println("Joe     \t\t43\t7\t50");
+System.out.println("William \t\t50\t8\t58");
+System.out.println("Mary Sue\t\t39\t10\t49");
+*/
+
+//task2--------------------------------------------
+/*
+System.out.println("\nThis is a long string that is the the "+
+                   "concatenation of two shorter strings.");
+
+System.out.println("The first computer was invented about " + 55 + 
+                   " years ago.");
+
+System.out.println("8 plus 5 is " + 8 + 5);
+System.out.println("8 plus 5 is " +(8 + 5));
+System.out.println(8 + 5 +" equals 8 plus 5.");
+*/
+
+//task 3---------------------------------------------
+/*
+String name="Hanan ";
+short age=21;
+double gpa=3.16;
+char gender='M';
+boolean foreigner=false;
+int ID=0146;
+
+System.out.println("Name: "+name);
+System.out.println("Age: "+age);
+System.out.println("GPA: "+gpa);
+System.out.println("Gender: "+gender);
+System.out.println("foreigner: "+foreigner);
+System.out.println("Student ID: "+ID);
+*/
+
+//task 4-----------------------------------------------
+
+//System.out.println((10+5)*(4-6)/4);
+
+//task 5-----------------------------------------------
+
+//System.out.print("*********\n*\t*\n*\t*\n*\t*\n*********");
+
+//task6------------------------------------------------
+/*
+System.out.print("Enter the Dollors: ");
+double dollors= input.nextDouble();
+System.out.println("Values in rupees: "+dollors*278.64);
+*/
+
+//task7-------------------------------------------------
+/*
+System.out.print("Enter the radius of the cylinder: ");
+double radius = input.nextDouble();
+System.out.print("Enter the height of the cylinder: ");
+double height= input.nextDouble();
+double pi=3.14159;
+double volume=pi*radius*radius*height;
+System.out.println("The volume of the cylinder is: "+volume);
+*/
+
+//task8-------------------------------------------------
+/*
+System.out.print("Enter a speed in miles per hour: ");
+int speed=input.nextInt();
+System.out.println("The converted value is: "+(speed*1.6));
+*/
+
+//task9-------------------------------------------------
+/*
+System.out.println("**Calculator**\n");
+System.out.print("Enter the First Number: ");
+int num1=input.nextInt();
+System.out.print("Enter the Arthmetic operator('+','-','/','*'): ");
+char sym=input.next().charAt(0);
+System.out.print("Enter the Second Number: ");
+int num2=input.nextInt();
+
+if(sym=='+'){
+System.out.println("The Addition is: "+(num1+num2));
+}
+else if(sym=='-'){
+System.out.println("The substraction is: "+(num1-num2));
+}
+else if(sym=='/'){
+System.out.println("The Division is: "+(num1/num2));
+}
+else if(sym=='*'){
+System.out.println("The Multiplication is: "+(num1*num2));
+}
+else{
+System.out.println("Again correct Arthmetic symbol ! ");
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
